e7feaa6844435779d03876281768e01f4f5cee70
Erdi Demir <erdi.demir@icloud.com>
2021-12-23 15:42:02 +0300

    Funtions Completed

---
+++ src/BootCamp/01Hafta/07ReturnTypes/ReturnTypesExample/CustomFunction.sql
@@ -0,0 +1,22 @@
+﻿CREATE FUNCTION [dbo].[FAyAd]
+(
+	@Ay TINYINT
+)
+RETURNS NVARCHAR(10)
+AS
+BEGIN
+	RETURN	CASE @Ay
+				WHEN 1 THEN 'Ocak'
+				WHEN 2 THEN 'Şubat'
+				WHEN 3 THEN 'Mart'
+				WHEN 4 THEN 'Nisan'
+				WHEN 5 THEN 'Mayıs'
+				WHEN 6 THEN 'Haziran'
+				WHEN 7 THEN 'Temmuz'
+				WHEN 8 THEN 'Ağustos'
+				WHEN 9 THEN 'Eylül'
+				WHEN 10 THEN 'Ekim'
+				WHEN 11 THEN 'Kasım'
+				WHEN 12 THEN 'Aralık'
+			END
+END
--- src/BootCamp/01Hafta/07ReturnTypes/ReturnTypesExample/FunctionSql.sql
+++ src/BootCamp/01Hafta/07ReturnTypes/ReturnTypesExample/FunctionSql.sql
@@ -1,1 +1,57 @@
 ﻿
+--String Functions
+
+SELECT  ASCII('D')
+
+SELECT CONCAT('W3Schools', '.com');
+
+SELECT DATALENGTH('W3Schools.com');
+
+SELECT REPLACE('SQL Tutorial', 'T', 'M');
+
+SELECT RTRIM(LTRIM('   SQL Tutorial    '));
+
+SELECT UPPER('   SQL Tutorial    ');
+
+SELECT Lower('   SQL Tutorial    ');
+
+--Numeric Functions
+
+SELECT AVG(Price) AS AveragePrice FROM Products
+
+SELECT COUNT(Id) AS NumberOfProducts FROM Products;
+
+SELECT MAX(Price) AS LargestPrice FROM Products;
+
+SELECT MIN(Price) AS LargestPrice FROM Products;
+
+SELECT ROUND(235.415, 2) AS RoundValue;
+
+SELECT SQUARE(64);
+
+--Date Functions
+
+SELECT CURRENT_TIMESTAMP;
+
+SELECT DATEADD(year, 1, '2017/08/25') AS DateAdd;
+
+SELECT DATEDIFF(year, '2017/08/25', '2011/08/25') AS DateDiff;
+
+SELECT DATEPART(year, '2017/08/25') AS DatePartInt;
+
+SELECT GETDATE();
+
+SELECT YEAR(GETDATE());
+SELECT MONTH(GETDATE());
+SELECT DAY(GETDATE());
+
+-- Advanced Functions
+
+SELECT CAST(25.65 AS int);
+
+SELECT ISNULL(NULL, 'W3Schools.com');
+
+SELECT CONVERT(int, 25.65);
+
+--Return the first non-null value in a list:
+SELECT COALESCE(NULL, NULL, NULL, 'W3Schools.com', NULL, 'Example.com');
