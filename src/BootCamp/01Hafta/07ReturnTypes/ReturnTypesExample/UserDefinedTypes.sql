93ae5da16d49ad2814458c178c739b62d1b8a703
Erdi Demir <erdi.demir@icloud.com>
2021-12-23 16:23:12 +0300

    User Defined Types Completed

---
+++ src/BootCamp/01Hafta/07ReturnTypes/ReturnTypesExample/UserDefinedTypes.sql
@@ -0,0 +1,12 @@
+﻿--Aliases for INT NOT NULL and VARCHAR(100)
+  CREATE TYPE id FROM INT NOT NULL;
+  CREATE TYPE location FROM VARCHAR(100);
+
+  CREATE TABLE person
+  (
+     id id PRIMARY KEY,
+     location location DEFAULT 'n/a'
+  );
+ 
+  INSERT INTO person VALUES (1, 'Paris, France');
+  --# 1 row(s) affected
