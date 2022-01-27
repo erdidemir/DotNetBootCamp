4bc791921824873fa3c1243c2d3883c9b2218857
Erdi Demir <erdi.demir@icloud.com>
2022-01-27 14:36:30 +0300

    RabbitMQ Example

---
+++ src/BootCamp/05week/03.RabbitMQ/Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("iskuyrugu", durable: true, false, false, null);
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume("iskuyrugu", false, consumer);
+                consumer.Received += (sender, e) =>
+                {
+                    
+                    Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()) + " alındı");
+                    channel.BasicAck(e.DeliveryTag, false);
+                };
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ src/BootCamp/05week/03.RabbitMQ/Publisher/Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("iskuyrugu", durable: true, false, false, null);
+                for (int i = 1; i <= 100; i++)
+                {
+                    byte[] bytemessage = Encoding.UTF8.GetBytes($"is - {i}");
+
+                    IBasicProperties properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "", routingKey: "iskuyrugu", basicProperties: properties, body: bytemessage);
+                }
+            }
+        }
+    }
+}
