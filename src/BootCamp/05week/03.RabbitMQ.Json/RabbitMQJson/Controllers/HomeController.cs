f81e4bbc86f15ebc09da48c4d8f2458de4f27a56
Erdi Demir <erdi.demir@icloud.com>
2022-01-27 14:56:46 +0300

    RabbitMQ.Json Completed

---
+++ src/BootCamp/05week/03.RabbitMQ.Json/RabbitMQJson/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQJson.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ConnectionFactory factory;
+        private readonly IConnection connection;
+
+        public HomeController()
+        {
+            factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            connection = factory.CreateConnection();
+        }
+
+        [HttpGet("/")]
+        public IActionResult Get()
+        {
+            var customer = new
+            {
+                Id = 1,
+                Name = "customer",
+                Email = "customer@customer.com"
+            };
+
+            using (var channel = this.connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "customer",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                var customerPayload = JsonSerializer.Serialize(customer);
+
+                var body = Encoding.UTF8.GetBytes(customerPayload);
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "customer",
+                    basicProperties: null,
+                    body: body
+                );
+            }
+
+            return Ok(customer);
+        }
+    }
+}
---
+++ src/BootCamp/05week/03.RabbitMQ.Json/RabbitMQJsonConsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQJsonConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume("customer", false, consumer);
+                consumer.Received += (sender, e) =>
+                {
+
+                    var body = e.Body.ToArray();
+                    var jsonString = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine($"Json receievd as {jsonString}");
+
+                    channel.BasicAck(e.DeliveryTag, false);
+                };
+                Console.Read();
+            }
+        }
+    }
+}
